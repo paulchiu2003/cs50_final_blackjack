b65f347c4322911e7d66c0b6082ba35946aa112c
paulchiu2003 <90163343+paulchiu2003@users.noreply.github.com>
2021-12-08 09:53:20 +0900

    Add files via upload

--- blackjack/app.py
+++ blackjack/app.py
@@ -3,11 +3,20 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from helpers import decision
 
 # Configure application
 app = Flask(__name__)
 
-#import database
+# Paul 1208 - Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Paul 1208 - Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# import database
 db = SQL("sqlite:///blackjack_table.db")
 
 # routing the buttons to different webpages
@@ -36,33 +45,38 @@ def das_excl_main():
 # execute database search for DAS included
 @app.route("/das_incl_submit", methods=["GET", "POST"])
 def das_incl_submit():
-    #save card inputs in variables
-    dealer = request.form.get("card_input_dealer")
-    card1 = request.form.get("card_input1")
-    card2 = request.form.get("card_input2")
-    total = card1 + card2
+    
+    # Paul 1208 - POST
+    if request.method == "POST":
+    
+        #save card inputs in variables
+        dealer = request.form.get("card_input_dealer")
+        card1 = request.form.get("card_input1")
+        card2 = request.form.get("card_input2")
+        total = card1 + card2
 
-    # if two cards are the both As
-    if card1 == 'A' and card2 == 'A':
-        # result = stand
-        return render_template("/das_incl_main_text.html", result='stand')
+        # if two cards are the both As
+        if card1 == 'A' and card2 == 'A':
+            # result = stand
+            return decision("stand")
 
-    # if one card is an A
-        #execute search in soft table
-    # else
-    else:
-        # execute search in hard table
+        # if one card is an A
+        elif card1 == 'A' or card2 == 'A':
+            #execute search in soft table
+            return decision("temp")
+        # else
+        else:
+            # execute search in hard table
 
-        # execute search for what the user should do
-        result = db.execute("SELECT ? FROM hard WHERE symbol = ?", dealer, total)
-        # if result is hit
-        # redirect to another site with extra input box
-        # if result is stand 
-        if result=='S':
-            #show result page with stand
-            return render_template("/das_incl_main_text.html", result='stand')
-        # if result is double/stand
-            # 
-        # if result is double/hit
-            #
-
+            # execute search for what the user should do
+            result = db.execute("SELECT ? FROM hard WHERE symbol = ?", dealer, total)
+            # if result is hit
+            # redirect to another site with extra input box
+            # if result is stand 
+            if result=='S':
+                #show result page with stand
+                return render_template("/das_incl_main_text.html", result='stand')
+            # if result is double/stand
+                # 
+            # if result is double/hit
+                #
