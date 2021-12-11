676efab4fa0a667cd6e67f9be0ce207fdfe689ae
paulchiu2003 <90163343+paulchiu2003@users.noreply.github.com>
2021-12-11 19:32:38 +0900

    Add files via upload

--- blackjack/app.py
+++ blackjack/app.py
@@ -36,11 +36,11 @@ def das_excl_intro():
 def das_incl_main_text():
     return render_template("das_incl_main_text.html")
 
-@app.route("/das_excl_main")
+@app.route("/das_excl_main_text")
 def das_excl_main():
-    return render_template("das_excl_main.html")
+    return render_template("das_excl_main_text.html")
 
-# Michelle @ Paul: I don't know where DAS vs NON-DAS fits into this part
+
 
 # execute database search for DAS included
 @app.route("/das_incl_submit", methods=["GET", "POST"])
@@ -136,4 +136,68 @@ def das_incl_submit():
                 # if others
                 else:
                     # show result page with cannot defined
+                    return decision("cannot defined (hard)")
+
+
+# execute database search for non-DAS included
+# only one difference compared to das_incl
+@app.route("/das_excl_submit", methods=["GET", "POST"])
+def das_excl_submit():
+
+    if request.method == "POST":
+
+        dealer = request.form.get("card_input_dealer")
+        card1 = request.form.get("card_input1")
+        card2 = request.form.get("card_input2")
+
+        if card1 == card2:
+            # the only difference compared to das_incl, change the search table from split-das to split_nondas
+            result = db.execute("SELECT * FROM split_nondas WHERE LeftCard = ? AND DealerCard = ?", card1, dealer)
+            if result[0]["Decision"] == 'Y':
+                return split("split")
+            elif result[0]["Decision"] == 'N':
+                total = int(card1) + int(card2)
+                total_text = str(total)
+                if total > 17:
+                    return dontsplit("do not split, but stand")
+                else:
+                    result = db.execute("SELECT * FROM hard WHERE Symbol = ? AND DealerCard = ?", total_text, dealer)
+                    if result[0]["Decision"] == 'H':
+                        return dontsplit("do not split, but hit")
+                    elif result[0]["Decision"] == 'S':
+                        return dontsplit("do not split, but stand")
+                    elif result[0]["Decision"] == 'D':
+                        return dontsplit("do not split, but double")
+                    else:
+                        return decision("cannot defined (hard)")
+            else:
+                return decision("cannot defined (split)")
+
+        elif card1 == 'A' or card2 == 'A':
+            result = db.execute("SELECT * FROM soft WHERE LeftCard = ? AND RightCard = ? AND DealerCard = ?", card1, card2, dealer)
+            if result[0]["Decision"] == 'S':
+                return stand("stand")
+            elif result[0]["Decision"] == 'Ds':
+                return double("double if allowed, otherwise stand")
+            elif result[0]["Decision"] == 'H':
+                return hit("hit")
+            elif result[0]["Decision"] == 'D':
+                return double("double if allowed, otherwise hit")
+            else:
+                return decision("cannot defined (soft)")
+
+        else:
+            total = int(card1) + int(card2)
+            total_text = str(total)
+            if total > 17:
+                return stand("stand")
+            else:
+                result = db.execute("SELECT * FROM hard WHERE Symbol = ? AND DealerCard = ?", total_text, dealer)
+                if result[0]["Decision"] == 'H':
+                    return hit("hit")
+                elif result[0]["Decision"] == 'S':
+                    return stand("stand")
+                elif result[0]["Decision"] == 'D':
+                    return double("double")
+                else:
                     return decision("cannot defined (hard)")
